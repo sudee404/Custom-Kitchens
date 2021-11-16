3d018b2a6de9b93ab322e72bbcd10fad926c289b
Mash <Mash@MASH>
2021-11-16 19:20:14 +0300

    Add project files.

---
+++ Custom Kitchens/Form1.Designer.vb
@@ -0,0 +1,185 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.gbxCustomerDetails = New System.Windows.Forms.GroupBox()
+        Me.lblRemarks = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.rmBudget = New System.Windows.Forms.TextBox()
+        Me.rmWidth = New System.Windows.Forms.TextBox()
+        Me.rmLength = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.gbxKitchenStyles = New System.Windows.Forms.GroupBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.gbxBox = New System.Windows.Forms.GroupBox()
+        Me.gbxCustomerDetails.SuspendLayout()
+        Me.gbxKitchenStyles.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'gbxCustomerDetails
+        '
+        Me.gbxCustomerDetails.Controls.Add(Me.gbxKitchenStyles)
+        Me.gbxCustomerDetails.Controls.Add(Me.lblRemarks)
+        Me.gbxCustomerDetails.Controls.Add(Me.Button1)
+        Me.gbxCustomerDetails.Controls.Add(Me.rmBudget)
+        Me.gbxCustomerDetails.Controls.Add(Me.rmWidth)
+        Me.gbxCustomerDetails.Controls.Add(Me.rmLength)
+        Me.gbxCustomerDetails.Controls.Add(Me.Label3)
+        Me.gbxCustomerDetails.Controls.Add(Me.Label2)
+        Me.gbxCustomerDetails.Controls.Add(Me.Label1)
+        Me.gbxCustomerDetails.Location = New System.Drawing.Point(12, 12)
+        Me.gbxCustomerDetails.Name = "gbxCustomerDetails"
+        Me.gbxCustomerDetails.Size = New System.Drawing.Size(353, 349)
+        Me.gbxCustomerDetails.TabIndex = 0
+        Me.gbxCustomerDetails.TabStop = False
+        Me.gbxCustomerDetails.Text = "Customer Details"
+        '
+        'lblRemarks
+        '
+        Me.lblRemarks.AutoEllipsis = True
+        Me.lblRemarks.AutoSize = True
+        Me.lblRemarks.Location = New System.Drawing.Point(94, 282)
+        Me.lblRemarks.Name = "lblRemarks"
+        Me.lblRemarks.Size = New System.Drawing.Size(0, 13)
+        Me.lblRemarks.TabIndex = 7
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(115, 236)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Enter"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'rmBudget
+        '
+        Me.rmBudget.Location = New System.Drawing.Point(138, 164)
+        Me.rmBudget.Name = "rmBudget"
+        Me.rmBudget.Size = New System.Drawing.Size(100, 20)
+        Me.rmBudget.TabIndex = 5
+        Me.rmBudget.Text = "25000"
+        '
+        'rmWidth
+        '
+        Me.rmWidth.Location = New System.Drawing.Point(138, 106)
+        Me.rmWidth.Name = "rmWidth"
+        Me.rmWidth.Size = New System.Drawing.Size(100, 20)
+        Me.rmWidth.TabIndex = 4
+        Me.rmWidth.Text = "200"
+        '
+        'rmLength
+        '
+        Me.rmLength.CausesValidation = False
+        Me.rmLength.Location = New System.Drawing.Point(138, 51)
+        Me.rmLength.Name = "rmLength"
+        Me.rmLength.Size = New System.Drawing.Size(100, 20)
+        Me.rmLength.TabIndex = 3
+        Me.rmLength.Text = "520"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(52, 171)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(47, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Budget :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(53, 109)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(41, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Width :"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(53, 54)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(46, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Length :"
+        '
+        'gbxKitchenStyles
+        '
+        Me.gbxKitchenStyles.Controls.Add(Me.gbxBox)
+        Me.gbxKitchenStyles.Controls.Add(Me.Button2)
+        Me.gbxKitchenStyles.Location = New System.Drawing.Point(0, 0)
+        Me.gbxKitchenStyles.Name = "gbxKitchenStyles"
+        Me.gbxKitchenStyles.Size = New System.Drawing.Size(353, 349)
+        Me.gbxKitchenStyles.TabIndex = 8
+        Me.gbxKitchenStyles.TabStop = False
+        Me.gbxKitchenStyles.Text = "Kitchen Style"
+        Me.gbxKitchenStyles.Visible = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(126, 293)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Proceed"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'gbxBox
+        '
+        Me.gbxBox.Location = New System.Drawing.Point(38, 19)
+        Me.gbxBox.Name = "gbxBox"
+        Me.gbxBox.Size = New System.Drawing.Size(278, 257)
+        Me.gbxBox.TabIndex = 7
+        Me.gbxBox.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.gbxCustomerDetails)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.gbxCustomerDetails.ResumeLayout(False)
+        Me.gbxCustomerDetails.PerformLayout()
+        Me.gbxKitchenStyles.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents gbxCustomerDetails As GroupBox
+    Friend WithEvents lblRemarks As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents rmBudget As TextBox
+    Friend WithEvents rmWidth As TextBox
+    Friend WithEvents rmLength As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents gbxKitchenStyles As GroupBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents gbxBox As GroupBox
+End Class
---
+++ Custom Kitchens/Form1.vb
@@ -0,0 +1,125 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim kitchenStyle() As String = {"Small L-shaped Kitchen", "Medium L-shaped Kitchen", "Large L-shaped Kitchen", "Small Straight Kitchen", "Medium Straight Kitchen", "Large Straight Kitchen", "Large Island Kitchen"}
+        Dim counterTops() As String = {"Laminate", "Wood Block", "Granite"}
+        Dim prices() As Integer = {3500, 4500, 5750, 6500, 7750, 9500, 8250, 9500, 12500, 1000, 1500, 2200, 5250, 6200, 8000, 6750, 8500, 11000, 12500, 15000, 23000}
+        Dim lengthsArray() As Integer = {250, 350, 450, 150, 350, 500, 450}
+        Dim widthsArray() As Integer = {180, 240, 350, 60, 60, 60, 450}
+        Dim msg As String
+
+
+        msg = verify()
+        lblRemarks.Text = msg
+        'checking if empty statement came back
+        If (msg <> (" ")) Then
+            'if not empty the form stays as is
+            gbxCustomerDetails.Visible = True
+        Else
+            'else  new groupbox is loaded and made visible
+            gbxKitchenStyles.Visible = True
+
+        End If
+
+        'get 
+        Dim chosenByDim() As String = getKitchenByDIm(rmLength.Text, rmWidth.Text, kitchenStyle, lengthsArray, widthsArray)
+        Dim chosen() As String = getKitchenByPrice(rmBudget.Text, chosenByDim, prices)
+        Dim top, left As Integer
+        top = 15
+        left = 6
+        For counter = 0 To (chosen.Length - 1)
+
+            Dim rdb As New RadioButton()
+            rdb.Text = chosen(counter)
+            rdb.Top = top
+            rdb.Left = left
+            rdb.Width = 147
+            gbxBox.Controls.Add(rdb)
+            top += 20
+            If (rdb.Text.Equals("")) Then
+                gbxBox.Controls.Remove(rdb)
+            End If
+
+        Next
+
+
+    End Sub
+
+    Function getKitchenByPrice(ByVal budget As Integer, ByVal chosen As String(), ByVal prices As Integer()) As String()
+        Dim x, y, count As Integer
+        Dim chosenStyles(7) As String
+        x = 0
+        y = 0
+
+        For counter = 0 To 6
+            count = 0
+            For counter1 = 1 To 3
+                If (budget > prices(x)) Then
+                    count += 1
+                End If
+                x += 1
+            Next
+            If (count >= 1) Then
+                chosenStyles(y) = chosen(counter)
+                y += 1
+            End If
+        Next
+        Return chosenStyles
+    End Function
+
+
+    Function getKitchenByDIm(ByVal length As Integer, ByVal width As Integer, ByVal kitchenstyle() As String,
+                             ByVal lengthArr() As Integer, ByVal widthArr() As Integer) As String()
+        Dim chosenStyles(7) As String
+        Dim cnt As Integer = 0
+        Dim cntprice As Integer = 0
+        For counter = 0 To 6
+            If (length > lengthArr(counter) And width > widthArr(counter)) Then
+                chosenStyles(cnt) = kitchenstyle(counter)
+                cnt += 1
+            End If
+        Next
+        Return chosenStyles
+
+    End Function
+    Function verify() As String
+        'this function verifies input by ensuring no characters except numbers can be accepted
+        'create a msg variable to hold return type message
+        Dim length, width, budget As Integer
+        Dim msg As String
+        'initialize with an empty text as default
+        msg = " "
+        'catch and raise exceptions with a try statement
+        Try
+            length = rmLength.Text
+            width = rmWidth.Text
+            budget = rmBudget.Text
+            'throw an exception when validateData() function returns as True
+            If (validateData(length, width, budget)) Then
+                Throw New Exception("Enter values within applicable ranges")
+            End If
+
+        Catch ex As InvalidCastException
+            'this code catches all conversion error exceptions that could break the code
+            msg = "Enter correct values"
+            Return msg
+        Catch ez As Exception
+            'this one catches all other exceptions including our own
+            msg = ez.Message
+            Return msg
+
+        End Try
+        'return the final message
+        'an empty string means a correct validation has occured
+        Return msg
+    End Function
+
+    Function validateData(ByVal lnth As Integer, ByVal width As Integer, ByVal budget As Integer) As Boolean
+        'This function validates data with an if statement and returns true for incorrect input and false for correct input
+        If (lnth < 150 Or width < 20 Or budget < 1000) Or (lnth > 600 Or width > 600 Or budget > 25000) Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+End Class
